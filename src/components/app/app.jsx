3ee288bea0a81d6578c4179d629b84ab34083c3b
fraankee <pchelkca@mail.ru>
2020-10-13 22:32:46 +0300

    Настройка марщрутизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Main from "../main/main";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
+import SignIn from "../sign-in/sign-in";
 
 const App = (props) => {
   const {placesFound} = props;
 
-  return <React.Fragment>
-    <Main placesFound={placesFound}
-    />
-  </React.Fragment>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesFound={placesFound} />
+        </Route>
+      </Switch>
+      <Switch >
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+      </Switch>
+      <Switch >
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+      <Switch >
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
